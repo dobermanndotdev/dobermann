65a2b184e70712232fdc4e3ca28bb64e903dc5c4
Firmino Changani <firmino.changani@gmail.com>
2023-11-30 21:59:44 +0000

    feat(#29): dashboard skeleton (#35)
    - wip: theming
    - wip: add account menu
    - wip: adjust the account menu
    - wip: fix the ci pipeline

--- webapp/app/(dashboard)/layout.tsx
+++ webapp/app/(dashboard)/layout.tsx
@@ -1,18 +1,13 @@
 import { PropsWithChildren } from "react";
-import Link from "next/link";
+
+import { Drawer } from "./(components)/Drawer";
+import { Header } from "./(components)/Header";
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
     <>
-      <header className="border px-10 py-4 flex justify-between">
-        <div>Dobermann</div>
-        <div>
-          <Link href="/api/auth/logout" className="btn btn-xs">
-            Logout
-          </Link>
-        </div>
-      </header>
-      <section className="px-10 py-4">{children}</section>
+      <Header />
+      <Drawer>{children}</Drawer>
     </>
   );
 }
