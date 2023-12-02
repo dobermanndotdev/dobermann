011822d20892fa850ef28abd0403edf67d062e53
Firmino Changani <firmino.changani@gmail.com>
2023-12-02 22:21:01 +0000

    feat(#8): add an event handler for the EndpointCheckSucceededEvent (#48)

--- internal/ports/amqp/endpoint_check_succeeded.go
+++ internal/ports/amqp/endpoint_check_succeeded.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/flowck/dobermann/backend/internal/adapters/events"
 	"github.com/flowck/dobermann/backend/internal/app"
+	"github.com/flowck/dobermann/backend/internal/app/command"
+	"github.com/flowck/dobermann/backend/internal/domain"
 )
 
 type EndpointCheckSucceededHandler struct {
@@ -12,7 +14,7 @@ type EndpointCheckSucceededHandler struct {
 }
 
 func (e EndpointCheckSucceededHandler) HandlerName() string {
-	return "EndpointCheckSucceeded_Handler"
+	return "EndpointCheckSucceeded_ResolveIncidentsHandler"
 }
 
 func (e EndpointCheckSucceededHandler) EventName() string {
@@ -20,6 +22,22 @@ func (e EndpointCheckSucceededHandler) EventName() string {
 }
 
 func (e EndpointCheckSucceededHandler) Handle(m *message.Message) error {
-	// Do nothing
+	event, err := events.NewEventFromMessage[events.EndpointCheckSucceededEvent](m)
+	if err != nil {
+		return err
+	}
+
+	monitorID, err := domain.NewIdFromString(event.MonitorID)
+	if err != nil {
+		return err
+	}
+
+	err = e.application.Commands.ResolveIncidents.Execute(m.Context(), command.ResolveIncidents{
+		MonitorID: monitorID,
+	})
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
