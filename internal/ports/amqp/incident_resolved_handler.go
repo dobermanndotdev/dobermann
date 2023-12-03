0c379280735edc8a6a1388b9505bfa76f4f5b51b
Firmino Changani <firmino.changani@gmail.com>
2023-12-03 18:52:09 +0000

    feat(#): add a handler to consume the event IncidentEesolved

--- internal/ports/amqp/incident_resolved_handler.go
+++ internal/ports/amqp/incident_resolved_handler.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/flowck/dobermann/backend/internal/adapters/events"
 	"github.com/flowck/dobermann/backend/internal/app"
+	"github.com/flowck/dobermann/backend/internal/app/command"
+	"github.com/flowck/dobermann/backend/internal/domain"
 )
 
 type IncidentResolvedHandler struct {
@@ -12,7 +14,7 @@ type IncidentResolvedHandler struct {
 }
 
 func (e IncidentResolvedHandler) HandlerName() string {
-	return "IncidentResolved_Handler"
+	return "IncidentResolved_NotifyOnIncidentResolved_Handler"
 }
 
 func (e IncidentResolvedHandler) EventName() string {
@@ -20,5 +22,29 @@ func (e IncidentResolvedHandler) EventName() string {
 }
 
 func (e IncidentResolvedHandler) Handle(m *message.Message) error {
+	event, err := events.NewEventFromMessage[events.IncidentResolvedEvent](m)
+	if err != nil {
+		return err
+	}
+
+	monitorID, err := domain.NewIdFromString(event.MonitorID)
+	if err != nil {
+		return err
+	}
+
+	incidentID, err := domain.NewIdFromString(event.IncidentID)
+	if err != nil {
+		return err
+	}
+
+	cmd := command.NotifyOnIncidentResolved{
+		IncidentID: incidentID,
+		MonitorID:  monitorID,
+	}
+	err = e.application.Commands.NotifyOnIncidentResolved.Execute(m.Context(), cmd)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
