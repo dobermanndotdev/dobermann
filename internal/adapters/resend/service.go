edd6cab13842994157111775b6ce5e70d6424681
Firmino Changani <firmino.changani@gmail.com>
2023-12-03 18:23:04 +0000

    feat(#8): add command to notify subscribers

--- internal/adapters/resend/service.go
+++ internal/adapters/resend/service.go
@@ -6,6 +6,7 @@ import (
 
 	resendsdk "github.com/resendlabs/resend-go/v2"
 
+	"github.com/flowck/dobermann/backend/internal/domain"
 	"github.com/flowck/dobermann/backend/internal/domain/account"
 	"github.com/flowck/dobermann/backend/internal/domain/monitor"
 )
@@ -13,12 +14,8 @@ import (
 // Service This interface is needed to allow the initialisation of either the concrete struct service or a mock
 // service to run the project in CI environments
 type Service interface {
-	SendEmailAboutIncident(
-		ctx context.Context,
-		user *account.User,
-		m *monitor.Monitor,
-		incident *monitor.Incident,
-	) error
+	SendEmailIncidentResolution(context.Context, *account.User, *monitor.Monitor, domain.ID) error
+	SendEmailAboutIncident(context.Context, *account.User, *monitor.Monitor, *monitor.Incident) error
 }
 
 type service struct {
@@ -53,6 +50,28 @@ Dobermann - Endpoint monitoring
 		From:    s.from,
 		To:      []string{user.Email().Address()},
 		Subject: fmt.Sprintf("[Dobermann] - New Incident for %s", m.EndpointUrl()),
+		Html:    body,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *service) SendEmailIncidentResolution(ctx context.Context, user *account.User, m *monitor.Monitor, incidentID domain.ID) error {
+	body := fmt.Sprintf(`
+Hi %s,</br>
+The last incident reported on the monitor %s has been resolved. </br>
+For more details please follow the link %s/monitors/%s/incidents/%s. </br>
+
+Dobermann - Endpoint monitoring
+`, user.FirstName(), m.EndpointUrl(), s.hostname, m.ID().String(), incidentID.String())
+
+	_, err := s.client.Emails.SendWithContext(ctx, &resendsdk.SendEmailRequest{
+		From:    s.from,
+		To:      []string{user.Email().Address()},
+		Subject: "[Dobermann] - Incident resolved",
 		Html:    body,
 	})
 	if err != nil {
