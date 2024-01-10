e5fe360c228d3914c6e7aae183dd4a88d49ca0e1
Firmino Changani <firmino.changani@gmail.com>
2024-01-10 18:21:47 +0000

    wip

--- cmd/endpoint_simulator/handlers.go
+++ cmd/endpoint_simulator/handlers.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/brianvoe/gofakeit"
+	"github.com/labstack/echo/v4"
+)
+
+const (
+	minLatencyMs = 50
+	maxLatencyMs = 60 * 1000
+)
+
+func randomFailureHandler(c echo.Context) error {
+	time.Sleep(time.Millisecond * time.Duration(gofakeit.Number(minLatencyMs, maxLatencyMs)))
+	return c.JSON(http.StatusOK, map[string]string{
+		"message": "ok",
+	})
+}
+
+func mainHandler(c echo.Context) error {
+	if c.QueryParam("timeout") == "true" {
+		time.Sleep(time.Second * 10)
+	}
+
+	if c.QueryParam("is_up") == "true" {
+		return c.NoContent(http.StatusOK)
+	}
+
+	if c.QueryParam("is_up") == "false" {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+
+	// Quasi-random path
+	s := getStatusCode()
+	return c.NoContent(s)
+}
